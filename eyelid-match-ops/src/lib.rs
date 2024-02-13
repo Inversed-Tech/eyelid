56499bd43e851ad0fdb070ebe01a466e84cc964b
teor <teor@riseup.net>
2024-02-13 12:06:06 +1000

    Add fundamental constants and document basic operations

--- eyelid-match-ops/src/lib.rs
+++ eyelid-match-ops/src/lib.rs
@@ -1,5 +1,16 @@
 //! Iris code matching operations library.
 //!
+//! Masking is applied to both iris codes before matching:
+//! ```text
+//! unmasked = mask_a & mask_b
+//! matching = (eye_a ^ eye_b) & unmasked
+//! ```
+//!
+//! A successful match has:
+//! ```text
+//! |matching| / |unmasked| >= IRIS_MATCH_NUMERATOR / IRIS_MATCH_DENOMINATOR
+//! ```
+//!
 //! This library has 3 core modules:
 //! [`plaintext`]: operations on raw bit vectors,
 //! [`encoded`]: the same operations on polynomial-encoded bit vectors,
@@ -9,6 +20,16 @@
 pub mod encoded;
 pub mod encrypted;
 pub mod plaintext;
+
+/// The length of a raw iris code or iris mask in bits.
+/// Most users have two of these codes, for their left and right eyes.
+pub const IRIS_BIT_LENGTH: usize = 2 * 20 * 160 * 2;
+
+/// The numerator of the bit match threshold for a successful iris match.
+pub const IRIS_MATCH_NUMERATOR: usize = 36;
+
+/// The denominator of the bit match threshold for a successful iris match.
+pub const IRIS_MATCH_DENOMINATOR: usize = 100;
 
 pub fn add(left: usize, right: usize) -> usize {
     left + right
