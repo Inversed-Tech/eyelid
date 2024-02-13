33c673035094e674e452961da0687b88807c6061
teor <teor@riseup.net>
2024-02-13 11:46:49 +1000

    Add core operations modules

--- eyelid-match-ops/src/lib.rs
+++ eyelid-match-ops/src/lib.rs
@@ -1,3 +1,15 @@
+//! Iris code matching operations library.
+//!
+//! This library has 3 core modules:
+//! [`plaintext`]: operations on raw bit vectors,
+//! [`encoded`]: the same operations on polynomial-encoded bit vectors,
+//! [`encrypted`]: the same operations on fully homomorphic encrypted, polynomial-encoded bit
+//!                vectors.
+
+pub mod encoded;
+pub mod encrypted;
+pub mod plaintext;
+
 pub fn add(left: usize, right: usize) -> usize {
     left + right
 }
---
+++ eyelid-match-ops/src/plaintext.rs
@@ -0,0 +1,1 @@
+//! Iris matching operations on raw bit vectors.
