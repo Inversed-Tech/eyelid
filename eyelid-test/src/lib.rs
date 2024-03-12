4d94f6fc7b3b45ca25dd909ea35bc85b1b99de54
teor <teor@riseup.net>
2024-03-12 10:09:37 +1000

    Remove Rust template test code

--- eyelid-test/src/lib.rs
+++ eyelid-test/src/lib.rs
@@ -2,18 +2,3 @@
 
 //#[macro_use]
 //extern crate static_assertions;
-
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
