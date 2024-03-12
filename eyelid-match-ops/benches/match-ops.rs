15e7773be2920bd1807340c95dcd0a5092a71f85
teor <teor@riseup.net>
2024-03-12 10:10:11 +1000

    Add plaintext match benchmark

---
+++ eyelid-match-ops/benches/match-ops.rs
@@ -0,0 +1,43 @@
+//! Benchmarks for matching operations.
+
+use eyelid_match_ops::plaintext::{
+    self,
+    test::gen::{random_iris_code, random_iris_mask},
+};
+
+use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion};
+
+// Configure Criterion:
+// Define one group for each equivalent operation, so we can compare their times.
+criterion_group! {
+    name = bench_full_match;
+    // This can be any expression that returns a `Criterion` object.
+    config = Criterion::default().sample_size(40);
+    // List full match implementations here.
+    targets = bench_plaintext_full_match
+}
+
+// List groups here.
+criterion_main!(bench_full_match);
+
+/// Run plaintext::is_iris_match() as a Criterion benchmark with random data.
+fn bench_plaintext_full_match(settings: &mut Criterion) {
+    // Setup: generate different random iris codes and masks
+    let eye_new = random_iris_code();
+    let mask_new = random_iris_mask();
+    let eye_store = random_iris_code();
+    let mask_store = random_iris_mask();
+
+    settings.bench_with_input(
+        BenchmarkId::new(
+            format!("Full iris match: plaintext"),
+            format!("random iris codes and masks"),
+        ),
+        &(eye_new, mask_new, eye_store, mask_store),
+        |benchmark, (eye_new, mask_new, eye_store, mask_store)| {
+            benchmark.iter_with_large_drop(|| {
+                plaintext::is_iris_match(eye_new, mask_new, eye_store, mask_store)
+            })
+        },
+    );
+}
