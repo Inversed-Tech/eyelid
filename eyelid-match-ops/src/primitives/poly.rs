33414b59292bec5565d3c32a402b3c2c58826d4a
emmorais <eduardo.morais@gmail.com>
2024-03-18 11:17:32 -0300

    Update constants

--- eyelid-match-ops/src/primitives/poly.rs
+++ eyelid-match-ops/src/primitives/poly.rs
@@ -8,8 +8,12 @@ use ark_poly::DenseUVPolynomial;
 use ark_ff::Zero;
 use ark_ff::One;
 
-/// The maximum polynomial degree
-const N: usize = 2048;
+/// The maximum exponent in the polynomial.
+pub const MAX_POLY_DEGREE: usize = 2048;
+
+/// The maximum length of the polynomial.
+/// One more than [`MAX_POLY_DEGREE`].
+pub const MAX_POLY_LEN: usize = MAX_POLY_DEGREE + 1;
 
 // Next we define 2 Finite Field using pre-computed primes and generators.
 // We could also consider generating primes dynamically.
@@ -48,32 +52,32 @@ pub type Fq66 = Fp128<MontBackend<Fq66Config, 2>>;
 fn test_cyclotomic_mul(){
     let p1 = rand_pol();
     let mut xnm1 = DensePolynomial::<Fq79>::zero();
-    xnm1.coeffs = [Fq79::zero(); N].to_vec();
-    xnm1.coeffs[N-1] = Fq79::one(); // Xˆ{N-1}, multiplying but it will rotate by N-1 and negate (except the first)
+    xnm1.coeffs = [Fq79::zero(); MAX_POLY_DEGREE].to_vec();
+    xnm1.coeffs[MAX_POLY_DEGREE-1] = Fq79::one(); // Xˆ{N-1}, multiplying but it will rotate by N-1 and negate (except the first)
     let res = cyclotomic_mul(&p1, &xnm1);
-    for i in 0..N-1 {
+    for i in 0..MAX_POLY_DEGREE-1 {
         assert_eq!(res[i], - p1[i+1]);
     }
+    assert_eq!(res[MAX_POLY_DEGREE-1], p1[0]);
 }
 
 /// Generates a cyclotomic polynomial with random coefficients in Fq79
 pub fn rand_pol() -> DensePolynomial::<Fq79>{
     let mut rng = ark_std::test_rng();
-    DensePolynomial::<Fq79>::rand(N-1, &mut rng)
+    DensePolynomial::<Fq79>::rand(MAX_POLY_DEGREE-1, &mut rng)
 }
 
 /// Use naive_mul followed by reduction mod XˆN - 1
 pub fn cyclotomic_mul(a: &DensePolynomial::<Fq79>, b: &DensePolynomial::<Fq79>) -> DensePolynomial::<Fq79>{
     let mut res = a.naive_mul(&b);
-    assert!(a.coeffs.len() <= N);
-    assert!(b.coeffs.len() <= N);
-    for i in 0..N {
+    assert!(a.coeffs.len() <= MAX_POLY_DEGREE);
+    assert!(b.coeffs.len() <= MAX_POLY_DEGREE);
+    for i in 0..MAX_POLY_DEGREE {
         // In the cyclotomic ring we have that XˆN = -1, therefore all elements from N to 2N are negated
-        if i + N < res.coeffs.len() {
-            res[i] = res[i] - res[i + N];
-            res[i + N] = Fq79::zero();
+        if i + MAX_POLY_DEGREE < res.coeffs.len() {
+            res[i] = res[i] - res[i + MAX_POLY_DEGREE];
+            res[i + MAX_POLY_DEGREE] = Fq79::zero();
         };
     }
-    assert!(res.coeffs.len() <= N);
     res
 }
