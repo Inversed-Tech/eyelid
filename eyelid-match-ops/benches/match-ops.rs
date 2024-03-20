1e98139c57e6e526bd65bd6ba01dc75ffc737bfd
Eduardo Morais <emmorais@users.noreply.github.com>
2024-03-20 13:28:47 -0300

    Naive cyclotomic multiplication over ark-poly (#6)
    * Naive cyclotomic multiplication
    
    * fix TODO using previous mul result instead of zero()
    
    * Implement first unit test
    
    * Add Cargo.lock
    
    * Clippy, documentation, rand_pol
    
    * Benchmark cyclotomic multiplication
    
    * Move bench
    
    * Remove file
    
    * Remove clones
    
    * Update eyelid-match-ops/benches/match-ops.rs
    
    Co-authored-by: teor <teor@riseup.net>
    
    * Update eyelid-match-ops/src/primitives.rs
    
    Co-authored-by: teor <teor@riseup.net>
    
    * Update eyelid-match-ops/src/primitives/poly.rs
    
    Co-authored-by: teor <teor@riseup.net>
    
    * Update eyelid-match-ops/src/primitives/poly.rs
    
    Co-authored-by: teor <teor@riseup.net>
    
    * Update constants
    
    * Remove unused const
    
    * Copy tests and constants from #7, apply suggestions, and fix bugs
    
    * Apply benchmark changes from #7
    
    * Describe canonical representation of negative num
    
    ---------
    
    Co-authored-by: teor <teor@riseup.net>

--- eyelid-match-ops/benches/match-ops.rs
+++ eyelid-match-ops/benches/match-ops.rs
@@ -5,25 +5,36 @@
 // TODO: move the macros to a separate module and allow missing docs only in that module.
 #![allow(missing_docs)]
 
-use eyelid_match_ops::plaintext::{
-    self,
-    test::gen::{random_iris_code, random_iris_mask},
-};
-
 use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion};
+
+use eyelid_match_ops::{
+    plaintext::{
+        self,
+        test::gen::{random_iris_code, random_iris_mask},
+    },
+    primitives::poly::{self, rand_poly, MAX_POLY_DEGREE},
+};
 
 // Configure Criterion:
 // Define one group for each equivalent operation, so we can compare their times.
 criterion_group! {
     name = bench_full_match;
     // This can be any expression that returns a `Criterion` object.
-    config = Criterion::default().sample_size(40);
+    config = Criterion::default().sample_size(50);
     // List full match implementations here.
     targets = bench_plaintext_full_match
 }
 
+criterion_group! {
+    name = bench_cyclotomic_multiplication;
+    // This can be any expression that returns a `Criterion` object.
+    config = Criterion::default().sample_size(50);
+    // List cyclotomic multiplication implementations here.
+    targets = bench_cyclotomic_mul
+}
+
 // List groups here.
-criterion_main!(bench_full_match);
+criterion_main!(bench_full_match, bench_cyclotomic_multiplication);
 
 /// Run plaintext::is_iris_match() as a Criterion benchmark with random data.
 fn bench_plaintext_full_match(settings: &mut Criterion) {
@@ -39,6 +50,23 @@ fn bench_plaintext_full_match(settings: &mut Criterion) {
         |benchmark, (eye_new, mask_new, eye_store, mask_store)| {
             benchmark.iter_with_large_drop(|| {
                 plaintext::is_iris_match(eye_new, mask_new, eye_store, mask_store)
+            })
+        },
+    );
+}
+
+/// Run cyclotomic_mul as a Criterion benchmark with random data.
+pub fn bench_cyclotomic_mul(settings: &mut Criterion) {
+    // Setup: generate random cyclotomic polynomials
+    let p1 = rand_poly(MAX_POLY_DEGREE);
+    let p2 = rand_poly(MAX_POLY_DEGREE);
+
+    settings.bench_with_input(
+        BenchmarkId::new("Cyclotomic multiplication: polynomial", "Random input"),
+        &(p1, p2),
+        |benchmark, (p1, p2)| {
+            benchmark.iter_with_large_drop(|| {
+                poly::cyclotomic_mul(p1, p2);
             })
         },
     );
