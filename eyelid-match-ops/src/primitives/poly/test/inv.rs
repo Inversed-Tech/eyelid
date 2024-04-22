1d1ad59e369cdae6ebafeb31a7c2bd6bbe34704e
teor <teor@riseup.net>
2024-04-22 16:29:26 +1000

    Allow non-invertible tiny polys in inverse tests

--- eyelid-match-ops/src/primitives/poly/test/inv.rs
+++ eyelid-match-ops/src/primitives/poly/test/inv.rs
@@ -6,7 +6,9 @@ use rand::Rng;
 use crate::primitives::poly::{Coeff, Poly};
 
 #[cfg(test)]
-use crate::primitives::poly::{inverse, FULL_RES_POLY_DEGREE};
+use crate::primitives::poly::{extended_gcd, inverse, FULL_RES_POLY_DEGREE};
+#[cfg(test)]
+use ark_poly::Polynomial;
 
 /// This sampling is similar to what will be necessary for YASHE KeyGen
 /// TODO: generate Gaussian distribution instead of "uniform"
@@ -26,10 +28,34 @@ pub fn sample<const MAX_POLY_DEGREE: usize>() -> Poly<MAX_POLY_DEGREE> {
 #[test]
 fn test_inverse() {
     let f = sample::<FULL_RES_POLY_DEGREE>();
+
     // REMARK: For our parameter choices it is very likely to find
     // the inverse in the first attempt.
-    // For small degree and coefficient modulus, the situation may change.
     let out = inverse(&f);
-    assert!(out.is_ok());
-    assert_eq!(f * out.expect("just checked ok"), Poly::one());
+
+    #[cfg(not(tiny_poly))]
+    let expect_msg = "unexpected non-invertible large polynomial";
+    #[cfg(tiny_poly)]
+    let expect_msg = "just checked ok";
+
+    if !cfg!(tiny_poly) || out.is_ok() {
+        assert_eq!(f * out.expect(expect_msg), Poly::one());
+    } else {
+        // For small degree and coefficient modulus, non-invertible polynomials are more likely.
+
+        // Check that `f` isn't invertible
+        let (_x, y, _d) = extended_gcd(&Poly::new_unreduced_poly_modulus_slow(), &f);
+        let fy = f * y;
+
+        // Since `f` is not invertible, `f * y` can't be `1`.
+        assert_ne!(
+            fy,
+            Poly::one(),
+            "incorrect inverse() impl: the inverse of f was y, because f * y == 1"
+        );
+        // Since all non-zero `Coeff` values *are* invertible in the integer field, `f * y` can't be a constant, either.
+        if fy != Poly::zero() {
+            assert_ne!(fy.degree(), 0, "incorrect inverse() impl: the inverse of f was y*(c^1), because f * y is a non-zero constant c");
+        }
+    }
 }
