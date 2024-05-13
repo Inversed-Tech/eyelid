d882cae79605ed7a8250fb6ed9a5b8fb7ea53362
teor <teor@riseup.net>
2024-05-13 13:22:19 +1000

    Make the crate constants alias the IrisBits config

--- eyelid-match-ops/src/lib.rs
+++ eyelid-match-ops/src/lib.rs
@@ -28,26 +28,29 @@ pub use conf::TestRes;
 #[cfg(tiny_poly)]
 pub use conf::TinyTest;
 
+// TODO: delete these constants after the crate has been updated to use IrisConf generic parameters.
+use iris::conf::IrisConf;
+
 /// The number of rows in a raw iris code or iris mask, in bits.
-pub const IRIS_COLUMN_LENGTH: usize = 80;
+pub const IRIS_COLUMN_LENGTH: usize = IrisBits::COLUMN_LENGTH;
 
 /// The number of columns in a raw iris code or iris mask, in bits.
-pub const IRIS_COLUMNS: usize = 160;
+pub const IRIS_COLUMNS: usize = IrisBits::COLUMNS;
 
 /// The length of a raw iris code or iris mask, in bits.
 /// Most users have two of these codes, for their left and right eyes.
-pub const IRIS_BIT_LENGTH: usize = IRIS_COLUMN_LENGTH * IRIS_COLUMNS;
+pub const IRIS_BIT_LENGTH: usize = IrisBits::BIT_LENGTH;
 
 /// The rotation limit when comparing irises.
 /// Each column is compared to the [`IRIS_ROTATION_LIMIT`] columns to its left and right.
-pub const IRIS_ROTATION_LIMIT: usize = 15;
+pub const IRIS_ROTATION_LIMIT: usize = IrisBits::ROTATION_LIMIT;
 
 /// The number of rotations used when comparing irises.
 /// This includes the comparison with no rotations.
-pub const IRIS_ROTATION_COMPARISONS: usize = IRIS_ROTATION_LIMIT * 2 + 1;
+pub const IRIS_ROTATION_COMPARISONS: usize = IrisBits::ROTATION_COMPARISONS;
 
 /// The numerator of the bit match threshold for a successful iris match.
-pub const IRIS_MATCH_NUMERATOR: usize = 36;
+pub const IRIS_MATCH_NUMERATOR: usize = IrisBits::MATCH_NUMERATOR;
 
 /// The denominator of the bit match threshold for a successful iris match.
-pub const IRIS_MATCH_DENOMINATOR: usize = 100;
+pub const IRIS_MATCH_DENOMINATOR: usize = IrisBits::MATCH_DENOMINATOR;
