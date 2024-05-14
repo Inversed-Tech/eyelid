b7f6ea660bd79da5be4b2fbecd486663b92fd698
teor <teor@riseup.net>
2024-05-14 10:20:10 +1000

    Replace config fields with trait constants, fix imports, whitespace

--- eyelid-match-ops/src/primitives/yashe.rs
+++ eyelid-match-ops/src/primitives/yashe.rs
@@ -1,11 +1,10 @@
 //! Implementation of YASHE cryptosystem
 //! `<https://eprint.iacr.org/2013/075.pdf>`
 
-use rand::Rng;
 use std::marker::PhantomData;
 
 use ark_ff::{BigInt, BigInteger, One, PrimeField, UniformRand};
-use rand::rngs::ThreadRng;
+use rand::{rngs::ThreadRng, Rng};
 use rand_distr::{Distribution, Normal};
 
 use crate::primitives::poly::Poly;
@@ -52,7 +51,7 @@ where
 }
 
 /// Message struct
-#[derive(Debug, Clone, PartialEq, Eq)]
+#[derive(Clone, Debug, Eq, PartialEq)]
 pub struct Message<C: YasheConf>
 where
     C::Coeff: From<i64> + From<u64>,
@@ -62,7 +61,7 @@ where
 }
 
 /// Ciphertext struct
-#[derive(Debug, Clone)]
+#[derive(Clone, Debug, Eq, PartialEq)]
 pub struct Ciphertext<C: YasheConf>
 where
     C::Coeff: From<i64> + From<u64>,
@@ -71,7 +70,7 @@ where
     pub c: Poly<C>,
 }
 
-impl<C: YasheConf> Yashe<C> 
+impl<C: YasheConf> Yashe<C>
 where
     C::Coeff: From<i64> + From<u64>,
 {
@@ -131,14 +130,14 @@ where
         public_key: PublicKey<C>,
         rng: &mut ThreadRng,
     ) -> Ciphertext<C> {
-        let logt = self.params.t.ilog2();
+        let logt = C::T.ilog2();
         let s = self.sample_err(rng);
         let e = self.sample_err(rng);
         let mut c = s * public_key.h + e;
 
-        let mut qdt: BigInt<2> = Coeff::MODULUS;
+        let mut qdt: BigInt<2> = C::Coeff::MODULUS;
         qdt.divn(logt);
-        let mqdt = m.m * Coeff::from(qdt);
+        let mqdt = m.m * C::Coeff::from(qdt);
         c += mqdt;
         c.truncate_to_canonical_form();
         Ciphertext { c }
@@ -154,9 +153,9 @@ where
 
     /// Decrypt a ciphertext
     pub fn decrypt(&self, c: Ciphertext<C>, private_key: PrivateKey<C>) -> Message<C> {
-        let logt = self.params.t.ilog2();
-        let q = Coeff::MODULUS;
-        let qm1d2 = Coeff::MODULUS_MINUS_ONE_DIV_TWO;
+        let logt = C::T.ilog2();
+        let q = C::Coeff::MODULUS;
+        let qm1d2 = C::Coeff::MODULUS_MINUS_ONE_DIV_TWO;
         let mut res = c.c * private_key.priv_key;
         for i in 0..C::MAX_POLY_DEGREE {
             // Convert coefficient to big integer
@@ -169,7 +168,7 @@ where
             }
             let coeff_res_u128 = self.convert_to_u128(coeff_res) / self.convert_to_u128(q);
 
-            let result = coeff_res_u128 % u128::from(self.params.t);
+            let result = coeff_res_u128 % u128::from(C::T);
             res[i] = result.into();
         }
         Message { m: res }
@@ -223,7 +222,7 @@ where
     pub fn sample_message(&self, rng: &mut ThreadRng) -> Message<C> {
         let mut res = Poly::non_canonical_zeroes(C::MAX_POLY_DEGREE);
         for i in 0..C::MAX_POLY_DEGREE {
-            let coeff_rand: u64 = rng.gen_range(0..self.params.t);
+            let coeff_rand: u64 = rng.gen_range(0..C::T);
             res[i] = coeff_rand.into();
         }
         res.truncate_to_canonical_form();
