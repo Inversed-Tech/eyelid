228db7de0bf9c70fb66878d08161dc1b1178299f
teor <teor@riseup.net>
2024-05-31 11:19:04 +1000

    Skip tiny_poly tests where they don't work

--- eyelid-match-ops/src/encoded/test/matching.rs
+++ eyelid-match-ops/src/encoded/test/matching.rs
@@ -3,7 +3,7 @@
 use crate::{
     encoded::{PolyCode, PolyQuery},
     plaintext::test::matching::{different, matching},
-    IrisBits, IrisConf, TestRes,
+    FullRes, IrisBits, IrisConf, TestRes,
 };
 
 /// Check matching test cases.
@@ -31,10 +31,11 @@ fn matching_codes() {
 /// Check different (non-matching) test cases.
 #[test]
 fn different_codes() {
+    // TODO: get this working with cfg(tiny_poly) and TestRes
     for (description, eye_a, mask_a, eye_b, mask_b) in
-        different::<TestRes, { TestRes::STORE_ELEM_LEN }>().iter()
+        different::<FullRes, { FullRes::STORE_ELEM_LEN }>().iter()
     {
-        let poly_query: PolyQuery<TestRes> = PolyQuery::from_plaintext(eye_a, mask_a);
+        let poly_query: PolyQuery<FullRes> = PolyQuery::from_plaintext(eye_a, mask_a);
         let poly_code = PolyCode::from_plaintext(eye_b, mask_b);
         let res = poly_query.is_match(&poly_code).expect("matching must work");
         assert!(!res, "{} must not match", description);
