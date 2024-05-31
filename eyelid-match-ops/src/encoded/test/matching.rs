195f30fff08b7691c5096d942c0e4a3b9cec89b6
teor <teor@riseup.net>
2024-05-31 11:16:21 +1000

    Update tests and make generic

--- eyelid-match-ops/src/encoded/test/matching.rs
+++ eyelid-match-ops/src/encoded/test/matching.rs
@@ -2,14 +2,26 @@
 
 use crate::{
     encoded::{PolyCode, PolyQuery},
-    plaintext::test::matching::{DIFFERENT, MATCHING},
+    plaintext::test::matching::{different, matching},
+    IrisBits, IrisConf, TestRes,
 };
 
 /// Check matching test cases.
 #[test]
 fn matching_codes() {
-    for (description, eye_a, mask_a, eye_b, mask_b) in MATCHING.iter() {
-        let poly_query = PolyQuery::from_plaintext(eye_a, mask_a);
+    for (description, eye_a, mask_a, eye_b, mask_b) in
+        matching::<TestRes, { TestRes::STORE_ELEM_LEN }>().iter()
+    {
+        let poly_query: PolyQuery<TestRes> = PolyQuery::from_plaintext(eye_a, mask_a);
+        let poly_code = PolyCode::from_plaintext(eye_b, mask_b);
+        let res = poly_query.is_match(&poly_code).expect("matching must work");
+        assert!(res, "{} must match", description);
+    }
+
+    for (description, eye_a, mask_a, eye_b, mask_b) in
+        matching::<IrisBits, { IrisBits::STORE_ELEM_LEN }>().iter()
+    {
+        let poly_query: PolyQuery<IrisBits> = PolyQuery::from_plaintext(eye_a, mask_a);
         let poly_code = PolyCode::from_plaintext(eye_b, mask_b);
         let res = poly_query.is_match(&poly_code).expect("matching must work");
         assert!(res, "{} must match", description);
@@ -19,8 +31,19 @@ fn matching_codes() {
 /// Check different (non-matching) test cases.
 #[test]
 fn different_codes() {
-    for (description, eye_a, mask_a, eye_b, mask_b) in DIFFERENT.iter() {
-        let poly_query = PolyQuery::from_plaintext(eye_a, mask_a);
+    for (description, eye_a, mask_a, eye_b, mask_b) in
+        different::<TestRes, { TestRes::STORE_ELEM_LEN }>().iter()
+    {
+        let poly_query: PolyQuery<TestRes> = PolyQuery::from_plaintext(eye_a, mask_a);
+        let poly_code = PolyCode::from_plaintext(eye_b, mask_b);
+        let res = poly_query.is_match(&poly_code).expect("matching must work");
+        assert!(!res, "{} must not match", description);
+    }
+
+    for (description, eye_a, mask_a, eye_b, mask_b) in
+        different::<IrisBits, { IrisBits::STORE_ELEM_LEN }>().iter()
+    {
+        let poly_query: PolyQuery<IrisBits> = PolyQuery::from_plaintext(eye_a, mask_a);
         let poly_code = PolyCode::from_plaintext(eye_b, mask_b);
         let res = poly_query.is_match(&poly_code).expect("matching must work");
         assert!(!res, "{} must not match", description);
