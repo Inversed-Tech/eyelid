ffb3eda2fb765eeb5bbc27431946eedeea5bbe00
teor <teor@riseup.net>
2024-06-03 12:44:37 +1000

    Skip test cases for tiny poly that can fail at random

--- eyelid-match-ops/src/plaintext/test/matching.rs
+++ eyelid-match-ops/src/plaintext/test/matching.rs
@@ -3,8 +3,8 @@
 use crate::{
     iris::conf::{IrisCode, IrisConf, IrisMask},
     plaintext::test::gen::{
-        codes, masks, occluded_iris_mask, random_iris_code, rotate_not_too_much, rotate_too_much,
-        set_iris_code, similar_iris_code, unset_iris_code, visible_iris_mask,
+        codes, masks, occluded_iris_mask, random_iris_code, rotate_not_too_much, set_iris_code,
+        similar_iris_code, unset_iris_code, visible_iris_mask,
     },
 };
 
@@ -110,10 +110,9 @@ pub fn different<C: IrisConf, const STORE_ELEM_LEN: usize>() -> Vec<(
     IrisMask<STORE_ELEM_LEN>,
 )> {
     let same_rand = random_iris_code();
-    let iris2 = random_iris_code();
-    let iris3 = rotate_too_much::<C, STORE_ELEM_LEN>(&iris2);
 
-    vec![
+    #[allow(unused_mut)]
+    let mut res = vec![
         (
             "set/unset, visible".to_string(),
             set_iris_code(),
@@ -128,21 +127,34 @@ pub fn different<C: IrisConf, const STORE_ELEM_LEN: usize>() -> Vec<(
             !same_rand,
             visible_iris_mask(),
         ),
-        (
+    ];
+
+    // In small polynomials these tests can fail by chance.
+    #[cfg(not(tiny_poly))]
+    {
+        use crate::plaintext::test::gen::rotate_too_much;
+
+        let iris2 = random_iris_code();
+        let iris3 = rotate_too_much::<C, STORE_ELEM_LEN>(&iris2);
+
+        res.push((
             "different".to_string(),
             same_rand,
             visible_iris_mask(),
             iris2,
             visible_iris_mask(),
-        ),
-        (
+        ));
+        #[cfg(not(tiny_poly))]
+        res.push((
             "too much rotated".to_string(),
             iris2,
             visible_iris_mask(),
             iris3,
             visible_iris_mask(),
-        ),
-    ]
+        ));
+    }
+
+    res
 }
 
 /// Check matching test cases.
