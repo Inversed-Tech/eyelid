63b664769ae458f1b8b56330a0f8d086df7ebefe
teor <teor@riseup.net>
2024-06-07 12:04:24 +1000

    Use full and middle where tiny won't work in plaintext tests

--- eyelid-match-ops/src/encoded/test/matching.rs
+++ eyelid-match-ops/src/encoded/test/matching.rs
@@ -4,7 +4,7 @@ use crate::{
     encoded::{PolyCode, PolyQuery},
     iris::conf::IrisConf,
     plaintext::test::matching::{different, matching},
-    MiddleBits, TestBits,
+    FullBits, MiddleBits, TestBits,
 };
 
 /// Check matching test cases.
@@ -16,7 +16,12 @@ fn matching_codes() {
         let poly_query: PolyQuery<TestBits> = PolyQuery::from_plaintext(eye_a, mask_a);
         let poly_code = PolyCode::from_plaintext(eye_b, mask_b);
         let res = poly_query.is_match(&poly_code).expect("matching must work");
-        assert!(res, "{} must match", description);
+        assert!(
+            res,
+            "{description} must match:\n\
+            query: {poly_query:?}\n\
+            code: {poly_code:?}"
+        );
     }
 
     for (description, eye_a, mask_a, eye_b, mask_b) in
@@ -25,22 +30,19 @@ fn matching_codes() {
         let poly_query: PolyQuery<MiddleBits> = PolyQuery::from_plaintext(eye_a, mask_a);
         let poly_code = PolyCode::from_plaintext(eye_b, mask_b);
         let res = poly_query.is_match(&poly_code).expect("matching must work");
-        assert!(res, "{} must match", description);
+        assert!(
+            res,
+            "{description} must match:\n\
+            query: {poly_query:?}\n\
+            code: {poly_code:?}"
+        );
     }
 }
 
 /// Check different (non-matching) test cases.
 #[test]
 fn different_codes() {
-    // TODO: get this working with cfg(tiny_poly) and TestRes
-    for (description, eye_a, mask_a, eye_b, mask_b) in
-        different::<TestBits, { TestBits::STORE_ELEM_LEN }>().iter()
-    {
-        let poly_query: PolyQuery<TestBits> = PolyQuery::from_plaintext(eye_a, mask_a);
-        let poly_code = PolyCode::from_plaintext(eye_b, mask_b);
-        let res = poly_query.is_match(&poly_code).expect("matching must work");
-        assert!(!res, "{} must not match", description);
-    }
+    // TODO: get this working with cfg(tiny_poly) and TestBits
 
     for (description, eye_a, mask_a, eye_b, mask_b) in
         different::<MiddleBits, { MiddleBits::STORE_ELEM_LEN }>().iter()
@@ -48,6 +50,25 @@ fn different_codes() {
         let poly_query: PolyQuery<MiddleBits> = PolyQuery::from_plaintext(eye_a, mask_a);
         let poly_code = PolyCode::from_plaintext(eye_b, mask_b);
         let res = poly_query.is_match(&poly_code).expect("matching must work");
-        assert!(!res, "{} must not match", description);
+        assert!(
+            !res,
+            "{description} must not match:\n\
+            query: {poly_query:?}\n\
+            code: {poly_code:?}"
+        );
+    }
+
+    for (description, eye_a, mask_a, eye_b, mask_b) in
+        different::<FullBits, { FullBits::STORE_ELEM_LEN }>().iter()
+    {
+        let poly_query: PolyQuery<FullBits> = PolyQuery::from_plaintext(eye_a, mask_a);
+        let poly_code = PolyCode::from_plaintext(eye_b, mask_b);
+        let res = poly_query.is_match(&poly_code).expect("matching must work");
+        assert!(
+            !res,
+            "{description} must not match:\n\
+            query: {poly_query:?}\n\
+            code: {poly_code:?}"
+        );
     }
 }
