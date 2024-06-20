0b87628db0ffa0888feb56042ef90d43ef574fb2
emmorais <eduardo.morais@gmail.com>
2024-06-20 11:20:54 -0300

    Improve README

--- eyelid-match-ops/src/encrypted/test/matching.rs
+++ eyelid-match-ops/src/encrypted/test/matching.rs
@@ -15,8 +15,7 @@ use colored::Colorize;
 fn convert_negative_coefficients<C: EncodeConf<PlainConf = LargeRes>>(
     polys: &mut [Poly<C::PlainConf>],
 ) {
-    #[allow(unused_mut)]
-    for mut poly in polys {
+    for poly in polys {
         Poly::coeffs_modify_non_zero(poly, |coeff: &mut <C::PlainConf as PolyConf>::Coeff| {
             let mut coeff_res = C::PlainConf::coeff_as_big_int(*coeff);
             if coeff_res > <C::PlainConf as YasheConf>::modulus_minus_one_div_two_as_big_int() {
