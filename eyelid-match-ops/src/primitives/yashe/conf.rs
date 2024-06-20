99531b2db3c1550c01e1e52e8c4211789cbd81f9
emmorais <eduardo.morais@gmail.com>
2024-06-20 18:45:42 -0300

    Fmt

--- eyelid-match-ops/src/primitives/yashe/conf.rs
+++ eyelid-match-ops/src/primitives/yashe/conf.rs
@@ -321,10 +321,10 @@ where
 impl YasheConf for LargeRes {
     type PolyBN = LargeResBN;
 
-    // TODO: max T now is more than enough, but on the other hand we lost a few bits of security. 
+    // TODO: max T now is more than enough, but on the other hand we lost a few bits of security.
     // Fixing it would require changing the max degree or increasing the initial noise.
     // Either way first we want to fine tune this parameter and minimize the size of T
-    // while keeping the encrypted encoding working. 
+    // while keeping the encrypted encoding working.
     // Larger values cause failures in the positive_multiplication_test().
     const T: u64 = 524288;
 }
